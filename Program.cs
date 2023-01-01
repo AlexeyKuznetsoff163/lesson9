71e6b94771b2b95ae3d1c6e3822561aaf4dbd61f
AlexeyKuznetsoff163 <118921804+AlexeyKuznetsoff163@users.noreply.github.com>
2023-01-01 21:41:28 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,22 @@
-﻿Console.WriteLine("Введите число M:");
+﻿//task1
+
+Console.WriteLine("Введите натуральное число больше 1: ");
+int number = int.Parse(Console.ReadLine());
+
+
+void NumberCounter (int number)
+{
+    if (number < 0) Console.Write($"{number} не натуральное число");
+    if (number == 0) return;
+    Console.Write("{0,4}", number);
+    NumberCounter (number - 1);
+}
+
+NumberCounter(number);
+
+//task2
+
+Console.WriteLine("Введите число M:");
 int numberM = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Введите число N:");
@@ -17,3 +35,37 @@ void GapNumberSum (int numberM, int numberN, int sum)
 }
 
 GapNumberSum(numberM, numberN, 0);
+
+//task3
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AkkermanFunction(m,n);
+
+
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n)); 
+}
+
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+
